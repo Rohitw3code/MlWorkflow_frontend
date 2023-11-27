55b4c02dee1c005699dca0bbda0fcdc455456cf6
Rishabh <108469021+Rishabh-raj-kumar@users.noreply.github.com>
2023-11-27 11:35:57 +0530

    updated ui

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 import Dataframe from "../componets/Dataframe";
 import MissingData from "../componets/MissingData";
 import DataTypeChange from "../componets/DataTypeChange";
@@ -14,8 +14,98 @@ export default function Home() {
   const [cols, setCols] = useState([]);
   const [sectionBVisible, setSectionBVisible] = useState(false);
   const [reloadDataTypeChange, setReloadDataTypeChange] = useState(false);
-  // const [selectedFile, setSelectedFile] = useState({"name":"No Selected"});
+  
+  const [active, setActive] = useState(null);
+  const observer = useRef(null);
 
+  const [dataTabs, setDataTabs] = useState([
+    {
+      id: "1",
+      tabTitle: "Data-PreProcessing",
+      moveto : '#section-A',
+      tabClass: "myCustomClass",
+      tabClicked: false
+    },
+    {
+      id: "2",
+      tabTitle: "Missing Values",
+      moveto : '#section-B',
+      tabClass: "myCustomClass",
+      tabClicked: false
+    },
+    {
+      id: "3",
+      tabTitle: "Data Encoding",
+      moveto : '#section-C',
+      tabClass: "myCustomClass",
+      tabClicked: false
+    },
+    {
+      id: "4",
+      tabTitle: "Features",
+      moveto : '#section-D',
+      tabClass: "myCustomClass",
+      tabClicked: false
+    },
+    {
+      id: "5",
+      tabTitle: "Train - Test",
+      moveto : '#section-E',
+      tabClass: "myCustomClass",
+      tabClicked: false
+    },
+    {
+      id: "6",
+      tabTitle: "ML - Algorithms",
+      moveto : '#section-F',
+      tabClass: "myCustomClass",
+      tabClicked: false
+    }
+  ]);
+
+  const NavLink = ({ id, tabTitle,moveTo, isActive, onClick }) => {
+    return (
+      <a
+        href={moveTo}
+        onClick={() => navigate(id)}
+        className={isActive ? ` inline-block w-56 h-full p-2 rounded-xl rounded-r-none text-sm md:text-xl ${color === '#ED9ED6' && 'bg-pink-200'} ${color === '#87C4FF' && 'bg-blue-200'}
+        ${color === '#9ADE7B' && 'bg-green-200'} ${color === '#FFCF96' && 'bg-yellow-200'} ${color === 'gradient--pink' && ' bg-gradient-to-r from-purple-200 to-pink-500'}
+        ${color === 'gradient--black' && 'bg-slate-800 text-white'}
+        ${color === 'aurora--bgr' && 'border-none backdrop-blur-md bg-white/30 custom--bg--pink text-slate-500'}` 
+        : " transition-all ease-in-out delay-200"}
+      >
+        {tabTitle}
+      </a>
+    );
+  };
+
+  const navigate = (id) => {
+    setActive(id);
+  };
+
+  useEffect(() => {
+    //create new instance and pass a callback function
+        observer.current = new IntersectionObserver((entries) => {
+          const visibleSection = entries.find((entry) => entry.isIntersecting)?.target;
+    //Update state with the visible section ID
+          if (visibleSection) {
+            setActive(visibleSection.id);
+          }
+        });
+    
+    //Get custom attribute data-section from all sections
+        const sections = document.querySelectorAll('[data-section]');
+    
+        sections.forEach((section) => {
+          observer.current.observe(section);
+        });
+    //Cleanup function to remove observer
+        return () => {
+          sections.forEach((section) => {
+            observer.current.unobserve(section);
+          });
+        };
+      }, []);
 
 
   const triggerReloadDataTypeChange = () => {
@@ -47,10 +137,13 @@ export default function Home() {
     setUserInput(event.target.value);
   };
 
+
   const [color,setColor] = useState('#87C4FF')
   return (
     <ThemeContext.Provider value={color}>
-    <div className={`relative ${color === '#ED9ED6' && 'bg-pink-200'} transition-all ease-linear delay-200 duration-300 ${color === '#87C4FF' && 'bg-blue-200'}
+      {console.log(active)}
+      <section className='relative bg-white/30'>
+    <div className={` ml-52 ${color === '#ED9ED6' && 'bg-pink-200'} transition-all ease-linear delay-200 duration-300 ${color === '#87C4FF' && 'bg-blue-200'}
       ${color === '#9ADE7B' && 'bg-green-200'} ${color === '#FFCF96' && 'bg-yellow-200'} ${color === 'gradient--pink' && 'bg-gradient-to-r from-indigo-300 via-purple-400 to-pink-400'}
       ${color === 'gradient--black' && 'bg-gradient-to-t from-slate-900 via-slate-800 to-slate-900 text-white'} ${color === 'aurora--bgr' && "bg-[url('/aurora1.jpg')]"} `}
       style={{
@@ -58,7 +151,26 @@ export default function Home() {
     backgroundSize: '100% 150%',
     backgroundPosition: 'center'
       }}>
-  <div className="fixed right-0 z-40 top-1/3 p-4 bg-white rounded-l-lg flex flex-col gap-2">
+  <div className={`w-52 oool, h-screen fixed top-0 left-0 flex flex-col justify-around items-center overflow-hidden ${color === '#ED9ED6' && 'bg-pink-600'} ${color === '#87C4FF' && 'bg-blue-600'}
+          ${color === '#9ADE7B' && 'bg-green-600'} ${color === '#FFCF96' && 'bg-yellow-600'} ${color === 'gradient--pink' && ' bg-gradient-to-r from-purple-600 to-pink-600'}
+          ${color === 'gradient--black' && 'bg-slate-700 text-white'}
+          ${color === 'aurora--bgr' && 'border-none backdrop-blur-md bg-black text-slate-300'}`}>
+    
+    <div className=' flex gap-3 place-items-center'>
+      <img src="/aurora2.jpg" className=' w-16 h-16 rounded-full'/>
+      <div className=' flex flex-col' style={{ fontFamily : 'Poppins'}}>
+          <h1 className='  font-semibold'>Rishabh RAJ</h1>
+          <p className=' font-mono text-sm'>ML LEARNER</p>
+      </div>
+    </div>
+    <ul className=' w-full flex flex-col gap-1'>
+      {dataTabs.map((item) => (
+        <li key={item.id} className=' w-full p-2 text-center'>
+          <NavLink {...item} isActive={active === item.id} moveTo={item.moveto} onClick={navigate} />
+        </li>
+      ))}
+    </ul>
+    <div className="z-40 w-full p-4 bg-white/80 columns-4 mx-auto">
       <button className=" bg-pink-500 w-8 h-8 rounded-full" onClick={() => setColor('#ED9ED6')}></button>
       <button className=" bg-blue-500 w-8 h-8 rounded-full" onClick={() => setColor('#87C4FF')}></button>
       <button className=" bg-teal-500 w-8 h-8 rounded-full" onClick={() => setColor('#9ADE7B')}></button>
@@ -67,13 +179,14 @@ export default function Home() {
       <button className=" bg-gradient-to-t from-slate-900 to-slate-700 w-8 h-8 rounded-full" onClick={() => setColor('gradient--black')}></button>
       <button className=" bg-gradient-to-t from-white/95 via-blue-300 to-blue-700 w-8 h-8 rounded-full" onClick={() => setColor('aurora--bgr')}></button>
   </div>
+  </div>
   <div className=" text-center mb-8">
     <div className=' w-fit  px-12 bg-white/50 shadow-xl m-auto rounded-b-full'>
   <h1 className={` flex items-center justify-center ${color === '#ED9ED6' && 'text-pink-800'} ${color === '#87C4FF' && 'text-blue-800'}
       ${color === '#9ADE7B' && 'text-green-800'} ${color === '#FFCF96' && 'text-yellow-800'} ${color === 'gradient--pink' && 'text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-pink-700'}`} style={{ fontFamily : 'Goza',fontSize : 'clamp(5vw,5rem,4vw)'}}>MLQuickFlow</h1>
       </div>
   </div>
-  <div className=' min-h-screen border-b-2 border-blue-500'>
+  <div id="1" data-section className=' min-h-screen border-b-2 border-blue-500'>
     <div className='p-2'>
       <div className=' w-full  lg:w-1/2 '>
       <img src="/data_preprocess.png"></img>
@@ -108,7 +221,7 @@ export default function Home() {
       </div>
       </div>
 
-      <div className={`section B top-8 backdrop-blur-lg ${color === 'aurora--bgr' && ""}`}>
+      <div id='2' data-section className={`section B top-8 backdrop-blur-lg ${color === 'aurora--bgr' && ""}`}>
       <div className='p-2 flex lg:justify-end'>
       <div className=' w-full  lg:w-1/2 '>
       <img src="/missing_value.png"></img>
@@ -123,7 +236,7 @@ export default function Home() {
         <DataTypeChange reloadDataTypeChange={reloadDataTypeChange} />
       </div>
 
-      <div className={`section D backdrop-blur-md ${color === 'aurora--bgr' && 'text-white'}`}>
+      <div  id="3" data-section className={`section D backdrop-blur-md ${color === 'aurora--bgr' && 'text-white'}`}>
       <div className='p-2 flex'>
       <div className=' w-full  lg:w-1/2 '>
       <img src="/data_encode.png"></img>
@@ -132,15 +245,15 @@ export default function Home() {
         <DataEncoding />
       </div>
 
-      <div className={`section E backdrop-blur-md ${color === 'aurora--bgr' && 'text-white'}`}>
+      <div id='4' data-section className={`section E backdrop-blur-md ${color === 'aurora--bgr' && 'text-white'}`}>
         <SelectFeatureTarget />
       </div>
 
-      <div className={`section E backdrop-blur-md ${color === 'aurora--bgr' && 'bg-white/50 p-5 rounded-lg text-gray-800'}`}>
+      <div id='5' data-section className={`section E backdrop-blur-md ${color === 'aurora--bgr' && 'bg-white/50 p-5 rounded-lg text-gray-800'}`}>
         <TrainTestSplit />
       </div>
 
-      <div className={`section E backdrop-blur-md ${color === 'aurora--bgr' && 'bg-white/50 mt-6 p-5 rounded-lg text-gray-800'}`}>
+      <div id='6' data-section className={`section E backdrop-blur-md ${color === 'aurora--bgr' && 'bg-white/50 mt-6 p-5 rounded-lg text-gray-800'}`}>
       <div className='p-2 flex'>
       <div className=' w-full '>
       <img src="/ml_algo.png"></img>
@@ -151,6 +264,7 @@ export default function Home() {
 
       
     </div>
+    </section>
     </ThemeContext.Provider>
   )
 }
